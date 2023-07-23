21503619ec17962f04c5d1875b87339eddb09e5b
sasakitimaru <84312254+sasakitimaru@users.noreply.github.com>
2023-07-23 21:24:27 +0900

    add server side amplify configure: this setting is mandatory

--- src/pages/api/addBookMark.ts
+++ src/pages/api/addBookMark.ts
@@ -1,16 +1,28 @@
-import { API, graphqlOperation } from 'aws-amplify';
-import { createBookmark } from '@/graphql/mutations';
-import { GraphQLResult } from '@aws-amplify/api-graphql';
-import Observable from 'zen-observable-ts';
+import { API, graphqlOperation } from "aws-amplify";
+import { createBookmark } from "@/graphql/mutations";
+import { GraphQLResult } from "@aws-amplify/api-graphql";
+import Observable from "zen-observable-ts";
+import awsExports from "@/aws-exports";
+import { Amplify } from "aws-amplify";
+Amplify.configure({ ...awsExports, ssr: true });
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default async (req: { body: any; }, res: { json: (arg0: GraphQLResult<any> | Observable<object> | {error: string}) => void; }) => {
+export default async (
+  req: { body: any },
+  res: {
+    json: (
+      arg0: GraphQLResult<any> | Observable<object> | { error: string }
+    ) => void;
+  }
+) => {
   try {
     const data = req.body;
-    const newBookmark = await API.graphql(graphqlOperation(createBookmark, { input: data }));
+    const newBookmark = await API.graphql(
+      graphqlOperation(createBookmark, { input: data })
+    );
     res.json(newBookmark);
   } catch (err) {
-    console.error('Error adding bookmark:', err);
-    res.json({ error: 'Error adding bookmark' });
+    console.error("Error adding bookmark:", err);
+    res.json({ error: "Error adding bookmark" });
   }
 };
--- src/pages/api/addComment.ts
+++ src/pages/api/addComment.ts
@@ -1,16 +1,27 @@
-import { API, graphqlOperation } from 'aws-amplify';
-import { createComment } from '@/graphql/mutations';
-import { GraphQLResult } from '@aws-amplify/api-graphql';
-import Observable from 'zen-observable-ts';
-
+import { API, graphqlOperation } from "aws-amplify";
+import { createComment } from "@/graphql/mutations";
+import { GraphQLResult } from "@aws-amplify/api-graphql";
+import Observable from "zen-observable-ts";
+import awsExports from "@/aws-exports";
+import { Amplify } from "aws-amplify";
+Amplify.configure({ ...awsExports, ssr: true });
 // eslint-disable-next-line import/no-anonymous-default-export
-export default async (req: { body: any; }, res: { json: (arg0: GraphQLResult<any> | Observable<object> | {error: string}) => void; }) => {
+export default async (
+  req: { body: any },
+  res: {
+    json: (
+      arg0: GraphQLResult<any> | Observable<object> | { error: string }
+    ) => void;
+  }
+) => {
   try {
     const data = req.body;
-    const newComment = await API.graphql(graphqlOperation(createComment, { input: data }));
+    const newComment = await API.graphql(
+      graphqlOperation(createComment, { input: data })
+    );
     res.json(newComment);
   } catch (err) {
-    console.error('Error adding comment:', err);
-    res.json({ error: 'Error adding comment' });
+    console.error("Error adding comment:", err);
+    res.json({ error: "Error adding comment" });
   }
 };
